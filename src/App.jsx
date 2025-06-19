93600cf3a7d6c4e401f61e88185de4b2d6d560c3
rev1s0r <petrik.baca@gmail.com>
2025-06-19 12:45:47 +0200

    running first version

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  return(
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <h1 className="">Find <span className="text-gradient">movies</span> you'll enjoy without the hassle</h1>
+          </header>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <p>Search</p>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
